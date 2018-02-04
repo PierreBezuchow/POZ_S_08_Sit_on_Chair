09d9d21f0c0b13bea0738cd4e720f88c3875377d
PierreBezuchow <axel@starosta.pl>
2018-02-04 12:23:00 +0100

    drobne poprawki w menu

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const gulp            = require('gulp');
+const browserSync     = require('browser-sync').create();
+const sass            = require('gulp-sass'); //sass
+const watch           = require('gulp-watch'); //watch
+const autoprefixer    = require('gulp-autoprefixer'); //auto prefixy
+const sourcemaps      = require('gulp-sourcemaps'); //sourcemapy
+const plumber         = require('gulp-plumber'); //zapobiera przerywaniu taskow
+const rename          = require('gulp-rename'); //zmiana nazwy wynikowych plikow
+const webpack         = require('webpack');
+
+
+
+const handleError = function(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
+
+
+gulp.task('browseSync', function() {
+    browserSync.init({
+        server: "./",
+        notify: false,
+        open: true //czy otwierac strone
+    });
+});
+
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/style.scss')
+        .pipe(plumber({ //przeciwdziala bledom w pipe ktore np przerywaja watch
+            errorHandler: handleError
+        }))
+        .pipe(sourcemaps.init()) //inicjalizacja sourcemap przed zabawa na plikach
+        .pipe(sass({
+            outputStyle: 'expanded' //nested, expanded, compact, compressed
+        }))
+        .pipe(autoprefixer({browsers: ["> 1%"]})) //autoprefixy https://github.com/postcss/autoprefixer#browsers
+        .pipe(rename({ //zamieniam wynikowy plik na style.min.css
+            suffix: '.min',
+            basename: 'style'
+        }))
+        .pipe(sourcemaps.write('.')) //po modyfikacjach na plikach zapisujemy w pamieci sourcemap
+        .pipe(gulp.dest("css")) //i calosc zapisujemy w dest
+        .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+
+gulp.task('es6', function(cb) {
+    return webpack(require('./webpack.config.js'), function(err, stats) {
+        if (err) throw err;
+        console.log(stats.toString());
+        cb();
+    })
+})
+
+
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['es6']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['sass', 'es6', 'browseSync', 'watch']);
